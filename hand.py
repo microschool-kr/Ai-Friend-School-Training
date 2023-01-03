5083c8c66fd3ae9c8d95b9262fade5155a1a6889
juchajam <79612680+juchajam@users.noreply.github.com>
2023-01-03 18:36:29 +0900

    Add files via upload

---
+++ hand.py
@@ -0,0 +1,46 @@
+import cv2  # OpenCV 라이브러리 import
+import sys  # sys 모듈 import
+import mediapipe as mp  # MediaPipe 패키지 import하고 mp라는 별칭으로 사용하겠다는 뜻.
+
+# MediaPipe 패키지에서 사용할 기능들.
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands  # 손 인식을 위한 객체
+
+cap = cv2.VideoCapture(0)  # 비디오 캡처 객체 생성
+
+if not cap.isOpened():  # 연결 확인
+    print("Camera is not opened")
+    sys.exit(1)  # 프로그램 종료
+
+hands = mp_hands.Hands()  # 손 인식 객체 생성
+
+while True:  # 무한 반복
+    res, frame = cap.read()  # 카메라 데이터 읽기
+
+    if not res:  # 프레임 읽었는지 확인
+        print("Camera error")
+        break  # 반복문 종료
+
+    frame = cv2.flip(frame, 1)  # 셀프 카메라처럼 좌우 반전
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # 미디어파이프에서 인식 가능한 색공간으로 변경
+    results = hands.process(image)  # 이미지에서 손을 찾고 결과를 반환
+
+    if results.multi_hand_landmarks:  # 손이 인식되었는지 확인
+        for hand_landmarks in results.multi_hand_landmarks:  # 반복문을 활용해 인식된 손의 주요 부분을 그림으로 그려 표현
+            mp_drawing.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style(),
+            )
+
+    cv2.imshow("MediaPipe Hands", frame)  # 영상을 화면에 출력.
+
+    key = cv2.waitKey(5) & 0xFF  # 키보드 입력받기
+    if key == 27:  # ESC를 눌렀을 경우
+        break  # 반복문 종료
+
+cv2.destroyAllWindows()  # 영상 창 닫기
+cap.release()  # 비디오 캡처 객체 해제
